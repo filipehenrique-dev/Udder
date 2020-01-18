fdf52f7d72e0634e2d266a9e8106f1c54d5a49cb
Filipe Henrique <filipe.henrique@me.com>
2020-01-18 12:49:32 -0800

    Initial Commit

---
+++ Udder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Filipe Henrique on 1/18/20.
+//  Copyright © 2020 Filipe Henrique. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
